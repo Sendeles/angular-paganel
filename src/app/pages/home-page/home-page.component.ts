f5e156beca13c4bd915b1435e0779b57c97a383d
Sendeles <keytareht@gmail.com>
2023-12-20 22:20:33 +0200

    home-page updated

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,54 @@
-import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import {AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild} from '@angular/core';
+import {CommonModule, isPlatformBrowser} from "@angular/common";
 import {HomePageRoutingModule} from "./home-page-routing.module";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
 
- image: string = './assets/images/paganelStudioExpedition.png'
+  image: string = './assets/images/paganelStudioExpedition.webp';
+  piece: string = './assets/images/piece.webp';
+  jumps1: string = './assets/images/jumps1.webp';
+  jumps2: string = './assets/images/jumps2.webp';
+  jumps3: string = './assets/images/jumps3.webp';
 
+  // isVisible: boolean = false;
+  // isVisibleFirst: boolean = false;
+  // isVisibleSecond: boolean = false;
+  // isVisibleThird: boolean = false;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+  }
+
+  // @ViewChild('imageContainer') imageContainer!: ElementRef;
+  //
+  ngAfterViewInit() {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     let observer = new IntersectionObserver((entries, observer) => {
+  //       entries.forEach((entry: any) => {
+  //         if (entry.isIntersecting) {
+  //           const imgClass = entry.target.getAttribute('class');
+  //           if (imgClass) {
+  //             if (imgClass.includes('first')) {
+  //               this.isVisibleFirst = true;
+  //             } else if (imgClass.includes('second')) {
+  //               this.isVisibleSecond = true;
+  //             } else if (imgClass.includes('third')) {
+  //               this.isVisibleThird = true;
+  //             }
+  //           }
+  //         }
+  //       });
+  //     }, {threshold: 0.5})
+  //
+  //     observer.observe(this.imageContainer.nativeElement);
+  //   }
+  }
 }
