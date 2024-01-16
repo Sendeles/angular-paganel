69040cf40349600bb910a125196ce0ca9ed1cf7e
Sendeles <keytareht@gmail.com>
2024-01-17 01:11:09 +0200

    update some words

--- src/app/shared/translations/translations.ts
+++ src/app/shared/translations/translations.ts
@@ -3,6 +3,11 @@ export let translations: any = {
     'en': 'Team',
     'ru': 'Команда',
     'ua': 'Команда'
+  },
+  'CALENDAR': {
+    'en': 'CALENDAR 2024',
+    'ru': 'КАЛЕНДАРЬ 2024',
+    'ua': 'КАЛЕНДАР 2024'
   }
 }
 
