b1d705cb9a5d3fae1f21270b55f8b424cc60667d
Sendeles <keytareht@gmail.com>
2024-02-20 20:22:43 +0200

    update lang changer

--- src/app/shared/layout/site-layout/site-layout.component.ts
+++ src/app/shared/layout/site-layout/site-layout.component.ts
@@ -3,14 +3,14 @@ import {RouterModule} from "@angular/router";
 import {fab, faYoutube} from "@fortawesome/free-brands-svg-icons";
 import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fas} from "@fortawesome/free-solid-svg-icons";
-import {NgClass} from "@angular/common";
+import {CommonModule, NgClass} from "@angular/common";
 import {LanguageServices} from "../../services/language.services";
 import {ButtonScrollTopComponent} from "../../components/button-scroll-top/button-scroll-top.component";
 
 @Component({
   selector: 'app-site-layout',
   standalone: true,
-  imports: [RouterModule, NgClass, ButtonScrollTopComponent],
+  imports: [RouterModule, NgClass, ButtonScrollTopComponent, CommonModule],
   templateUrl: './site-layout.component.html',
   styleUrl: './site-layout.component.scss'
 })
@@ -29,7 +29,7 @@ export class SiteLayoutComponent {
 
 // Переменная для хранения текущего выбранного языка
   currentLanguage = 'ua';
-  // languages = ['ua', 'en', 'ru'];
+  languages = ['ua', 'en', 'ru'];
 
   constructor(
     public languageService: LanguageServices
@@ -42,11 +42,11 @@ export class SiteLayoutComponent {
     // Доступ к кликнутому элементу
     const el = event.target as HTMLElement;
 
-    // // Удаляем выбранный язык из массива, если он уже есть
-    // this.languages = this.languages.filter(l => l !== language);
-    //
-    // // Добавляем выбранный язык в начало массива
-    // this.languages.unshift(language);
+    // Удаляем выбранный язык из массива, если он уже есть
+    this.languages = this.languages.filter(l => l !== language);
+
+    // Добавляем выбранный язык в начало массива
+    this.languages.unshift(language);
 
     // Определение, находится ли кликнутый элемент в пределах элемента списка .hasSubMenuLanguage
     const hasSubMenuLanguage = el.closest('.hasSubMenuLanguage');
@@ -64,5 +64,9 @@ export class SiteLayoutComponent {
         submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
       }
     }
+  }
+
+  getFilteredLanguages() {
+    return this.languages.filter(lang => lang !== this.currentLanguage);
   }
 }
